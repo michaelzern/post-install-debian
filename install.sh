a8211cb8fe13b674d3a7e421a9c8063349d7a65b
mikez <11712290+michaelzern@users.noreply.github.com>
2021-12-22 10:30:46 -0600

    added

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 apt-get update  # To get the latest package lists
-apt-get install <package name> -y
-#etc.
+# apt-get install <package name> -y
+apt-get install htop -y
+apt-get install tmux -y
+apt-get install bmon -y
+apt-get install nano -y
+
+#etc
