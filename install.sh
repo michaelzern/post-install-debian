fae5711ef9cad7c940e2b640acfca371f6a0e5d4
mikez <11712290+michaelzern@users.noreply.github.com>
2021-12-23 07:58:58 -0600

    new

--- install.sh
+++ install.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
-LIST_OF_APPS="htop tmux bmon nano"
-
 apt-get update
-apt-get install -y $LIST_OF_APPS
 
 #etc
+
+set -eu -o pipefail # fail on error and report it, debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
+
+echo installing the must-have pre-requisites
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+    htop
+    tmux
+    bmon
+    curl
+    nano
+    wget
+
+EOF
+)
+
+echo installing the nice-to-have pre-requisites
+echo you have 5 seconds to proceed ...
+echo or
+echo hit Ctrl+C to quit
+echo -e "\n"
+sleep 6
+
+sudo apt-get install -y tig
