60020c554ded671c4a274564d4b5dc5655f5e2fe
mikez <11712290+michaelzern@users.noreply.github.com>
2021-12-23 06:22:29 -0600

    a

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-apt-get update  # To get the latest package lists
-# apt-get install <package name> -y
-apt-get install htop -y
-apt-get install tmux -y
-apt-get install bmon -y
-apt-get install nano -y
+
+LIST_OF_APPS="htop tmux bmon nano"
+
+apt-get update
+apt-get install -y $LIST_OF_APPS
 
 #etc
