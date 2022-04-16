da74b816863a13dfb0651d5ecd584da9561377b2
mikez <11712290+michaelzern@users.noreply.github.com>
2022-04-15 22:20:48 -0500

    fix $HOME for .bashrc

--- install.sh
+++ install.sh
@@ -5,11 +5,12 @@
 ## Update packages and Upgrade system
 sudo apt update && sudo apt upgrade -y
 
-package="tmux screenfetch docker.io htop tmux bmon curl nano wget"
+package="screenfetch docker.io htop tmux bmon curl nano wget"
 
-echo installing the nice-to-have pre-requisites
+echo Installing the nice-to-have pre-requisites
+echo
 echo you have 5 seconds to proceed ...
-echo or
+echo
 echo hit Ctrl+C to quit
 echo -e "\n"
 sleep 6
@@ -22,7 +23,7 @@ done
 sudo usermod -aG docker $USER
 
 # 1. Set the file to write to
-file='$HOME/.bashrc'
+file=$HOME/.bashrc
 
 # 2. Append text with '>>'
 cat <<EOT >> $file
