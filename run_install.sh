5ebadb939e6962914091f6f30f4b421adb645b0b
Michael Zern <11712290+michaelzern@users.noreply.github.com>
2023-04-16 14:29:33 -0500

    fix checksums

--- run_install.sh
+++ run_install.sh
@@ -12,7 +12,7 @@ curl -O --remote-name-all https://raw.githubusercontent.com/michaelzern/post-ins
 curl -O https://raw.githubusercontent.com/michaelzern/post-install-debian/main/checksums.sha256
 
 # Verify the files using the checksums
-if (cd post-install && sha256sum -c checksums.sha256); then
+if sha256sum -c checksums.sha256; then
   echo "Checksums match. Proceeding with installation."
 else
   echo "Checksums do not match. Aborting installation."
